4af6c752fbd76d4d930c04d67930e85813f65aaf
Anisa Musaeva <anisa.musaeva@hotmail.com>
2022-11-27 00:58:12 +0300

    добавленно событие для прочих устройств

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-document.querySelector("header img").addEventListener("mouseover", function () {
+document.querySelector("header img").addEventListener("touchend", function () {
     document
         .querySelector("header img")
         .setAttribute("style", "display: none;");
     nav.setAttribute("style", "display:block");
 });
-document.querySelector("nav img").addEventListener("mouseover", function (){
+document.querySelector("nav img").addEventListener("touchend", function () {
     nav.setAttribute("style", "display:none");
     document
         .querySelector("header img")
