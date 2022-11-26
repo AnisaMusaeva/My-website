e0a9ee268c70154a09d46a68c4914f09ba13e6ce
Anisa Musaeva <anisa.musaeva@hotmail.com>
2022-11-27 00:30:52 +0300

    test версия для работы навигации

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function Nav_close(){
         .querySelector("header img")
         .setAttribute("style", "display: inline;");
 }
+
+
+document.querySelector("header img").addEventListener("mouseover", function () {
+    document
+        .querySelector("header img")
+        .setAttribute("style", "display: none;");
+    nav.setAttribute("style", "display:block");
+});
+document.querySelector("nav img").addEventListener("mouseover", function (){
+    nav.setAttribute("style", "display:none");
+    document
+        .querySelector("header img")
+        .setAttribute("style", "display: inline;");
+});
