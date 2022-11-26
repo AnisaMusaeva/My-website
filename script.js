d785f482ab8f2ddd1a7d8604a441066933f240f5
Anisa Musaeva <anisa.musaeva@hotmail.com>
2022-11-27 00:47:25 +0300

    удалены события нажатия мыши

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const nav = document.querySelector(".none");
-function Nav_open(){
-    document.querySelector("header img").setAttribute("style", "display: none;");
-    nav.setAttribute("style", "display:block")
-}
-function Nav_close(){
-    nav.setAttribute("style", "display:none");
-    document
-        .querySelector("header img")
-        .setAttribute("style", "display: inline;");
-}
-
 
 document.querySelector("header img").addEventListener("mouseover", function () {
     document
