ff1721c140c6df1cbe0e752fa7c342bdf8dad157
Anisa Musaeva <anisa.musaeva@hotmail.com>
2022-11-27 13:50:44 +0300

    обработчик события (click)

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-document.querySelector("header img").addEventListener("touchend", function () {
+document.querySelector("header img").addEventListener("click", function () {
     document
         .querySelector("header img")
         .setAttribute("style", "display: none;");
-    nav.setAttribute("style", "display:block");
+    document.querySelector(".none").setAttribute("style", "display:block");
 });
-document.querySelector("nav img").addEventListener("touchend", function () {
-    nav.setAttribute("style", "display:none");
+document.querySelector("nav img").addEventListener("click", function () {
+    document.querySelector(".none").setAttribute("style", "display:none");
     document
         .querySelector("header img")
         .setAttribute("style", "display: inline;");
